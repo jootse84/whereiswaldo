3c1743536092ec2a95b91ea317843f5937847f17
Jose <jootse.84@gmail.com>
2017-02-23 16:49:45 -0500

    Histogram SVM with surprisingly good results

--- SVC.py
+++ SVC.py
@@ -1,70 +1,78 @@
 from __future__ import print_function
-# import argparse
+from sklearn import svm, metrics
+import argparse
 import imghdr
 import cv2
 import os, os.path
 import numpy as np
 import random
+import helper
 
-'''
+def grayscaleSVC(pos_files_path, neg_files_path):
+    positives = []
+    negatives = []
+    for name in os.listdir(pos_files_path):
+        image = cv2.imread(pos_files_path + name, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+        positives.append({
+            'image': np.reshape(image, -1),
+            'label': 1
+        })
+    for name in os.listdir(neg_files_path):
+        image = cv2.imread(neg_files_path + name, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+        negatives.append({
+            'image': np.reshape(image, -1),
+            'label': 0
+        })
+    predict(positives, negatives)
+
+def histogramSVC(pos_files_path, neg_files_path):
+    positives = []
+    negatives = []
+    for name in os.listdir(pos_files_path):
+        image = cv2.imread(pos_files_path + name)
+        positives.append({
+            'image': helper.color_histogram(image),
+            'label': 1
+        })
+    for name in os.listdir(neg_files_path):
+        image = cv2.imread(neg_files_path + name)
+        negatives.append({
+            'image': helper.color_histogram(image),
+            'label': 0
+        })
+    predict(positives, negatives)
+
+
+def predict(positives, negatives):
+    data = np.concatenate((positives, negatives[:126]), axis=0)
+    random.shuffle(data)
+    test = data[:10]
+    data = data[10:]
+    img_data = []
+    lab_data = []
+    for image in data:
+        img_data.append(image['image'])
+        lab_data.append(image['label'])
+    img_test = []
+    lab_test = []
+    for image in test:
+        img_test.append(image['image'])
+        lab_test.append(image['label'])
+    classifier = svm.SVC(gamma=0.001)
+    classifier.fit(img_data, lab_data)
+    predicted = classifier.predict(img_test)
+    print(metrics.classification_report(lab_test, predicted))
+
+def SVC(type):
+    if type == "grayscale":
+        grayscaleSVC('./training/grayscale/positives/', './training/grayscale/negatives/')
+    else:
+        histogramSVC('./training/positives/', './training/negatives/')
+
+
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required = True, help = "Path to the image")
+ap.add_argument("-t", "--type", required = True, help = "Type of the SVC approach - 'histogram' or 'grayscale'")
 args = vars(ap.parse_args())
-'''
-
-positives = []
-negatives = []
-path = './training/grayscale/positives/'
-for name in os.listdir(path):
-    image = cv2.imread(path + name, cv2.CV_LOAD_IMAGE_GRAYSCALE)
-    positives.append({
-        'image': np.reshape(image, -1),
-        'label': 1
-    })
-
-path = './training/grayscale/negatives/'
-for name in os.listdir(path):
-    image = cv2.imread(path + name, cv2.CV_LOAD_IMAGE_GRAYSCALE)
-    negatives.append({
-        'image': np.reshape(image, -1),
-        'label': 0
-    })
-
-data = np.concatenate((positives, negatives[:126]), axis=0)
-random.shuffle(data)
-
-test = data[:10]
-data = data[10:]
-
-img_data = []
-lab_data = []
-for image in data:
-    img_data.append(image['image'])
-    lab_data.append(image['label'])
-
-img_test = []
-lab_test = []
-for image in test:
-    img_test.append(image['image'])
-    lab_test.append(image['label'])
-
-from sklearn import svm, metrics
-
-classifier = svm.SVC(gamma=0.001)
-classifier.fit(img_data, lab_data)
-
-predicted = classifier.predict(img_test)
-
-print(metrics.classification_report(lab_test, predicted))
-
-'''
-image = cv2.imread('./training/grayscale/positives/flipped_solo_waldo1.png')
-print(image.shape)
-print("width: {} pixels".format(image.shape[1]))
-print("height: {} pixels".format(image.shape[0]))
-print("channels: {}".format(image.shape[2]))
-cv2.imshow("Image", image)
-cv2.imwrite("newimage.jpg", image)
-'''
+SVC(args['type'])
 
 cv2.waitKey(0)
--- helper.py
+++ helper.py
@@ -1,3 +1,7 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
 def create_mirrors():
     # expand our dataset by creating mirrors of our images
     for folder in ['positives', 'negatives']:
@@ -17,3 +21,32 @@ def create_grayscales():
                 dest_path = "./training/grayscale/%s/" % folder
                 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
                 cv2.imwrite(dest_path + name, gray_image)
+
+# http://www.ippatsuman.com/2014/08/13/day-and-night-an-image-classifier-with-scikit-learn/
+def show_color_histogram(image, title):
+    # create as a feature vector the histogram including the RGB colors of an image
+    chanels = cv2.split(image) # list of 3 arrays, each one representing a color
+    colors = ("b", "g", "r")
+    plt.figure()
+    plt.title("Histogram %s" % title)
+    plt.xlabel("Bins")
+    plt.ylabel("# of Pixels")
+
+    for (chan, color) in zip(chanels, colors):
+        hist = cv2.calcHist([chan], [0], None, [256], [0, 256])
+        plt.plot(hist, color = color)
+        plt.xlim([0, 256])
+
+    plt.show()
+
+def color_histogram(image):
+    result = []
+    for chan in cv2.split(image):
+        hist = cv2.calcHist([chan], [0], None, [256], [0, 256])
+        result.append(np.reshape(hist, -1))
+    return np.reshape(result, -1).astype(int)
+
+#image = cv2.imread("./training/positives/flipped_solo_waldo3.png")
+#show_color_histogram(image, "solo_waldo3")
+image = cv2.imread("./training/negatives/no35.png")
+cv2.waitKey(0)
